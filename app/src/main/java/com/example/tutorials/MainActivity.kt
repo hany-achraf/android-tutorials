0f53370785047ea9140b845442c73543a9572fc3
hany-achraf <hany212mohamed@gmail.com>
2023-11-19 16:57:45 +0400

    INTENT SERVICE #29

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,61 +1,34 @@
 package com.example.tutorials
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.TaskStackBuilder
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.provider.CalendarContract.Colors
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
 
-    val CHANNEL_ID = "channelID"
-    val CHANNEL_NAME = "channelName"
-    val NOTIFICATION_ID = 0
-
-    lateinit var btnShowNotification: Button;
-
+    lateinit var btnStartService: Button
+    lateinit var btnStopService: Button
+    lateinit var tvServiceStatus: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        createNotificationChannel()
 
-        val intent = Intent(this, MainActivity::class.java)
-        val pendingIntent = TaskStackBuilder.create(this).run {
-            addNextIntentWithParentStack(intent)
-            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
+        tvServiceStatus = findViewById(R.id.tvServiceStatus)
 
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Awesome Notification")
-            .setContentText("This is the content text")
-            .setSmallIcon(R.drawable.ic_notification)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(pendingIntent)
-            .build()
-
-        val notificationManager = NotificationManagerCompat.from(this)
-
-        btnShowNotification = findViewById(R.id.btnShowNotification)
-        btnShowNotification.setOnClickListener {
-            notificationManager.notify(NOTIFICATION_ID, notification)
-        }
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH).apply {
-                lightColor = R.color.colorPrimary
-                enableLights(true)
+        btnStartService = findViewById(R.id.btnStartService)
+        btnStartService.setOnClickListener {
+            Intent(this, MyIntentService::class.java).also {
+                startService(it)
+                tvServiceStatus.text = "Service running..."
             }
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
+        }
+
+        btnStopService = findViewById(R.id.btnStopService)
+        btnStopService.setOnClickListener {
+            MyIntentService.stopService()
+            tvServiceStatus.text = "Service stopped..."
         }
     }
 }
