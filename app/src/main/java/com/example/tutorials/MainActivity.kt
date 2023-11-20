05945305d7ce455860128382e3a128a6d5b15817
hany-achraf <hany212mohamed@gmail.com>
2023-11-20 18:03:31 +0400

    DRAG & DROP #31

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,50 +1,75 @@
 package com.example.tutorials
 
-import android.content.Intent
+import android.content.ClipData
+import android.content.ClipDescription
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.view.DragEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tutorials.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btnStartService: Button
-    lateinit var btnStopService: Button
-    lateinit var tvServiceStatus: TextView
-
-    lateinit var etDataString: EditText
-    lateinit var btnSendData: Button
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var dragListener: View.OnDragListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        tvServiceStatus = findViewById(R.id.tvServiceStatus)
+        binding.dragView.setOnLongClickListener {
+            val label = "This is clip text"
+            val mimeTypes = arrayOf<String>(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val clipDescription = ClipDescription(label, mimeTypes)
+            val clipData = ClipData(clipDescription, ClipData.Item(label))
 
-        btnStartService = findViewById(R.id.btnStartService)
-        btnStartService.setOnClickListener {
-            Intent(this, MyService::class.java).also {
-                startService(it)
-                tvServiceStatus.text = "Service is running..."
+            val shadowBuilder = View.DragShadowBuilder(it)
+
+            it.startDragAndDrop(clipData, shadowBuilder, it, 0)
+            it.visibility = View.INVISIBLE
+            true
+        }
+
+        dragListener = View.OnDragListener { view, event ->
+            when (event.action) {
+                DragEvent.ACTION_DRAG_STARTED ->
+                    event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+                DragEvent.ACTION_DRAG_ENTERED -> {
+                    view.invalidate()
+                    true
+                }
+                DragEvent.ACTION_DRAG_LOCATION -> true
+                DragEvent.ACTION_DRAG_EXITED -> {
+                    view.invalidate()
+                    true
+                }
+                DragEvent.ACTION_DROP -> {
+                    val item = event.clipData.getItemAt(0)
+                    val data = item.text
+                    Toast.makeText(this, data, Toast.LENGTH_SHORT).show()
+
+                    val v = event.localState as View
+                    val owner = v.parent as ViewGroup
+                    owner.removeView(v)
+
+                    val destination = view as LinearLayout
+                    destination.addView(v)
+
+                    v.visibility = View.VISIBLE
+                    true
+                }
+                DragEvent.ACTION_DRAG_ENDED -> {
+                    view.invalidate()
+                    true
+                }
+                else -> false
             }
         }
 
-        btnStopService = findViewById(R.id.btnStopService)
-        btnStopService.setOnClickListener {
-            Intent(this, MyService::class.java).also {
-                stopService(it)
-                tvServiceStatus.text = "Service is stopped..."
-            }
-        }
-
-        btnSendData = findViewById(R.id.btnSendData)
-        btnSendData.setOnClickListener {
-            Intent(this, MyService::class.java).also {
-                etDataString = findViewById(R.id.etDataString)
-                it.putExtra("EXTRA_DATA", etDataString.text.toString())
-                startService(it)
-            }
-        }
+        binding.llTop.setOnDragListener(dragListener)
+        binding.llBottom.setOnDragListener(dragListener)
     }
 }
