602f5043a0de688769697d8fc71084c621856a29
hany-achraf <hany212mohamed@gmail.com>
2023-11-21 17:35:18 +0400

    Coroutine Contexts #4

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,27 +1,36 @@
 package com.example.tutorials
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.RadioButton
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tutorials.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private val TAG = "Coroutines"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSubmit.setOnClickListener {
-            val checkedMeat = findViewById<RadioButton>(binding.rgMeat.checkedRadioButtonId).text.toString()
-            binding.tvOrder.text = "You ordered burger with: \n$checkedMeat" +
-                    "${if (binding.cbCheese.isChecked) "\nCheese" else ""}" +
-                    "${if (binding.cbOnion.isChecked) "\nOnion" else ""}" +
-                    "${if (binding.cbSalad.isChecked) "\nSalad" else ""}"
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.d(TAG, "Starting coroutine in thread ${Thread.currentThread().name}")
+            val res = doNetworkCall()
+            withContext(Dispatchers.Main) {
+                Log.d(TAG, "Setting text in thread ${Thread.currentThread().name}")
+                binding.tvDummy.text = res
+            }
         }
+    }
+
+    suspend fun doNetworkCall(): String {
+        delay(3000L)
+        return "OK"
     }
 }
