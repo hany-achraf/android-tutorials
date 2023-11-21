845f9f044a85fca138d2b7a7ff7bce0fc602e612
hany-achraf <hany212mohamed@gmail.com>
2023-11-21 16:21:14 +0400

    CHECKBOX & RADIOBUTTON #12

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,29 +1,27 @@
 package com.example.tutorials
 
 import android.content.Intent
-import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tutorials.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var airplaneModeReceiver: AirplaneModeReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
-            airplaneModeReceiver = AirplaneModeReceiver()
-            registerReceiver(airplaneModeReceiver, it)
+        binding.btnSubmit.setOnClickListener {
+            val checkedMeat = findViewById<RadioButton>(binding.rgMeat.checkedRadioButtonId).text.toString()
+            binding.tvOrder.text = "You ordered burger with: \n$checkedMeat" +
+                    "${if (binding.cbCheese.isChecked) "\nCheese" else ""}" +
+                    "${if (binding.cbOnion.isChecked) "\nOnion" else ""}" +
+                    "${if (binding.cbSalad.isChecked) "\nSalad" else ""}"
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unregisterReceiver(airplaneModeReceiver)
     }
 }
