32506cb2194212fdaca2015407300b290eeace81
hany-achraf <hany212mohamed@gmail.com>
2023-11-21 19:43:55 +0400

    Async and Await #7

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -5,11 +5,10 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "Coroutines"
@@ -17,40 +16,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val job = GlobalScope.launch(Dispatchers.Default) {
-            withTimeout(10L) {
-                for (i in 30..40) {
-                    if (isActive) {
-                        Log.d(TAG, "Result for i = $i: ${fib(i)}")
-                    }
-                }
-            }
-//            for (i in 30..40) {
-//                if (isActive) {
-//                    Log.d(TAG, "Result for i = $i: ${fib(i)}")
-//                }
+//        GlobalScope.launch(Dispatchers.IO) {
+//            val time = measureTimeMillis {
+//                var res1 = doNetworkCall1()
+//                var res2 = doNetworkCall2()
+//                Log.d(TAG, res1)
+//                Log.d(TAG, res2)
 //            }
-            Log.d(TAG, "End long running calculations")
-        }
-
-//        runBlocking {
-//            delay(1000L)
-//            job.cancel()
-//            Log.d(TAG, "Main Thread is continuing...")
+//            Log.d(TAG, "This code took $time ms.")
 //        }
+
+//        GlobalScope.launch(Dispatchers.IO) {
+//            val time = measureTimeMillis {
+//                var res1 = doNetworkCall1()
+//                Log.d(TAG, res1)
+//                var res2 = doNetworkCall2()
+//                Log.d(TAG, res2)
+//            }
+//            Log.d(TAG, "This code took $time ms.")
+//        }
+
+//        GlobalScope.launch(Dispatchers.IO) {
+//            val time = measureTimeMillis {
+//                var res1: String? = null
+//                var res2: String? = null
+//
+//                val job1 = launch { res1 = doNetworkCall1() }
+//                val job2 = launch { res2 = doNetworkCall2() }
+//
+//                job1.join()
+//                job2.join()
+//
+//                Log.d(TAG, res1.toString())
+//                Log.d(TAG, res2.toString())
+//            }
+//            Log.d(TAG, "This code took $time ms.")
+//        }
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val time = measureTimeMillis {
+                val res1 = async { doNetworkCall1() }
+                val res2 = async { doNetworkCall2() }
+                Log.d(TAG, res1.await())
+                Log.d(TAG, res2.await())
+            }
+            Log.d(TAG, "This code took $time ms.")
+        }
     }
 
-//    private fun fib(i: Int): Int {
-//        if (i < 2) return 1
-//        return fib(i - 1) + fib(i - 2)
-//    }
+    private suspend fun doNetworkCall1(): String {
+        delay(3000L)
+        return "OK #1"
+    }
 
-    /** Solution with memoization */
-    private var map = HashMap<Int, Int>()
-    private fun fib(i: Int): Int {
-        if (map.containsKey(i)) return map[i] ?: 0
-        if (i < 2) return 1
-        map[i] = fib(i - 1) + fib(i - 2)
-        return map[i] ?: 0
+    private suspend fun doNetworkCall2(): String {
+        delay(3000L)
+        return "OK #2"
     }
 }
