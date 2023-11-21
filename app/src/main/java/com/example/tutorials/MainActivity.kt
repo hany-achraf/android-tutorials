d798193016318d4bdef2a391951f5b320c6333dd
hany-achraf <hany212mohamed@gmail.com>
2023-11-21 19:23:44 +0400

    Jobs, Waiting, Cancellation #6

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -3,34 +3,54 @@ package com.example.tutorials
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tutorials.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
     private val TAG = "Coroutines"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        GlobalScope.launch(Dispatchers.IO) {
-            Log.d(TAG, "Starting coroutine in thread ${Thread.currentThread().name}")
-            val res = doNetworkCall()
-            withContext(Dispatchers.Main) {
-                Log.d(TAG, "Setting text in thread ${Thread.currentThread().name}")
-                binding.tvDummy.text = res
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            withTimeout(10L) {
+                for (i in 30..40) {
+                    if (isActive) {
+                        Log.d(TAG, "Result for i = $i: ${fib(i)}")
+                    }
+                }
             }
+//            for (i in 30..40) {
+//                if (isActive) {
+//                    Log.d(TAG, "Result for i = $i: ${fib(i)}")
+//                }
+//            }
+            Log.d(TAG, "End long running calculations")
         }
+
+//        runBlocking {
+//            delay(1000L)
+//            job.cancel()
+//            Log.d(TAG, "Main Thread is continuing...")
+//        }
     }
 
-    suspend fun doNetworkCall(): String {
-        delay(3000L)
-        return "OK"
+//    private fun fib(i: Int): Int {
+//        if (i < 2) return 1
+//        return fib(i - 1) + fib(i - 2)
+//    }
+
+    /** Solution with memoization */
+    private var map = HashMap<Int, Int>()
+    private fun fib(i: Int): Int {
+        if (map.containsKey(i)) return map[i] ?: 0
+        if (i < 2) return 1
+        map[i] = fib(i - 1) + fib(i - 2)
+        return map[i] ?: 0
     }
 }
