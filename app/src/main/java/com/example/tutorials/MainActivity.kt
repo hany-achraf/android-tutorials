543d2678c625c7f5e77105da928980ad52176d73
hany-achraf <hany212mohamed@gmail.com>
2023-11-21 20:11:41 +0400

    lifecycleScope and viewModelScope #8

--- app/src/main/java/com/example/tutorials/MainActivity.kt
+++ app/src/main/java/com/example/tutorials/MainActivity.kt
@@ -1,76 +1,41 @@
 package com.example.tutorials
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.Dispatchers
+import androidx.lifecycle.lifecycleScope
+import com.example.tutorials.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     private val TAG = "Coroutines"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-//        GlobalScope.launch(Dispatchers.IO) {
-//            val time = measureTimeMillis {
-//                var res1 = doNetworkCall1()
-//                var res2 = doNetworkCall2()
-//                Log.d(TAG, res1)
-//                Log.d(TAG, res2)
-//            }
-//            Log.d(TAG, "This code took $time ms.")
-//        }
-
-//        GlobalScope.launch(Dispatchers.IO) {
-//            val time = measureTimeMillis {
-//                var res1 = doNetworkCall1()
-//                Log.d(TAG, res1)
-//                var res2 = doNetworkCall2()
-//                Log.d(TAG, res2)
-//            }
-//            Log.d(TAG, "This code took $time ms.")
-//        }
-
-//        GlobalScope.launch(Dispatchers.IO) {
-//            val time = measureTimeMillis {
-//                var res1: String? = null
-//                var res2: String? = null
-//
-//                val job1 = launch { res1 = doNetworkCall1() }
-//                val job2 = launch { res2 = doNetworkCall2() }
-//
-//                job1.join()
-//                job2.join()
-//
-//                Log.d(TAG, res1.toString())
-//                Log.d(TAG, res2.toString())
-//            }
-//            Log.d(TAG, "This code took $time ms.")
-//        }
-
-        GlobalScope.launch(Dispatchers.IO) {
-            val time = measureTimeMillis {
-                val res1 = async { doNetworkCall1() }
-                val res2 = async { doNetworkCall2() }
-                Log.d(TAG, res1.await())
-                Log.d(TAG, res2.await())
+        binding.btnStartActivity.setOnClickListener {
+//            GlobalScope.launch {
+            lifecycleScope.launch { /** viewModelScope works in the same way */
+                while (true) {
+                    /** Might need to consider checking `isActive` same as done with fib */
+                    delay(1000L)
+                    Log.d(TAG, "Still running...")
+                }
             }
-            Log.d(TAG, "This code took $time ms.")
+
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity, SecondActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
         }
-    }
-
-    private suspend fun doNetworkCall1(): String {
-        delay(3000L)
-        return "OK #1"
-    }
-
-    private suspend fun doNetworkCall2(): String {
-        delay(3000L)
-        return "OK #2"
     }
 }
